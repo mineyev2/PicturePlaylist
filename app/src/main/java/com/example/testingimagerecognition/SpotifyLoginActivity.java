37f7d40e0c0b2139db05e477ceca3564f02989cc
mineyev2 <mineyev2@illinois.edu>
2019-12-04 00:13:15 -0600

    logging in with spotify android sdk, and also added kaaes library to do the rest of the work. Error right now is the redirect uri

--- app/src/main/java/com/example/testingimagerecognition/SpotifyLoginActivity.java
+++ app/src/main/java/com/example/testingimagerecognition/SpotifyLoginActivity.java
@@ -23,6 +23,12 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -30,11 +36,17 @@ import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
 import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
+/*
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+
+ */
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -42,8 +54,19 @@ import java.util.Map;
 
 import java.net.HttpURLConnection;
 
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Album;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+
 
 public class SpotifyLoginActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1337;
+    private static final String REDIRECT_URI = "cs125.cs.illinois.edu://callback";
+    private static final String CLIENT_ID = "a0f1241498384db5ba98d07fd0ec4b99";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,31 +89,95 @@ public class SpotifyLoginActivity extends AppCompatActivity {
         });
 
         try {
+            /*
             String urlBase = "https://accounts.spotify.com/authorize";
+
             URL url = new URL(urlBase + "?client_id=a0f1241498384db5ba98d07fd0ec4b99"
                     + "&response_type=code" + "&redirect_uri=https://cs125.cs.illinois.edu/"
                     + "scope=user-read-private user-read-email");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+
             con.setRequestMethod("GET");
             con.setDoOutput(true);
             //DataOutputStream out = new DataOutputStream(url);
             //out.writeBytes(ParameterStringBuilder);
+
+             */
+
+
         } catch (Exception e) {
             Log.i("Error", e.toString());
         }
-
-
-
     }
 
     private void spotifyAuthentification() {
         System.out.println("Sign In Button");
+
+
+        loggingIn();
+
+
+        SpotifyApi api = new SpotifyApi();
+        api.setAccessToken("myAccessToken");
+        SpotifyService spotify = api.getService();
+
+        spotify.getAlbum("2dIGnmEIy1WZIcZCFSj6i8", new Callback<Album>() {
+            @Override
+            public void success(Album album, retrofit.client.Response response) {
+                Log.d("Album success", album.name);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.d("Album failure", error.toString());
+            }
+        });
+
         finish();
     }
 
     private void showHelpScreen() {
         System.out.println("Help");
         startActivity(new Intent(this, HelpActivity.class));
+    }
+
+    private void loggingIn() {
+        AuthenticationRequest.Builder builder =
+                new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+
+        builder.setScopes(new String[]{"streaming"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            System.out.println("request code works");
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    // Handle successful response
+                    System.out.println("success");
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    // Handle error response
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+            }
+        }
     }
 
 }
