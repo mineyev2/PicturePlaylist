02f04e29378744b2cb4b7f900524b8757bf91ffc
Andrea Roy <akroy2@illinois.edu>
2019-12-06 12:08:24 -0600

    searches every keyword passed in the intent and appends all song results to the song list instance variable

--- app/src/main/java/com/example/testingimagerecognition/PlaylistActivity.java
+++ app/src/main/java/com/example/testingimagerecognition/PlaylistActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +13,8 @@ import java.util.Map;
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
 import kaaes.spotify.webapi.android.models.Album;
+import kaaes.spotify.webapi.android.models.Track;
+import kaaes.spotify.webapi.android.models.TracksPager;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
@@ -38,23 +42,32 @@ public class PlaylistActivity extends AppCompatActivity {
 
         token = getIntent().getStringExtra("token");
         keywords = (HashMap<String, Float>) getIntent().getSerializableExtra("keywords");
+        songs = new ArrayList<>();
         SpotifyApi api = new SpotifyApi();
 
         api.setAccessToken(token);
         SpotifyService spotify = api.getService();
 
-        spotify.getAlbum("2dIGnmEIy1WZIcZCFSj6i8", new Callback<Album>() {
-            @Override
-            public void success(Album album, Response response) {
-                System.out.println("Album success" + album.name);
-            }
+        System.out.println(keywords);
+        for (String word: keywords.keySet()) {
+            spotify.searchTracks(word, new Callback<TracksPager>() {
+                @Override
+                public void success(TracksPager pager, Response response) {
+                    for (Track track: pager.tracks.items) {
+                        songs.add(track.id);
+                    }
+                    System.out.println(songs);
+                }
 
-            @Override
-            public void failure(RetrofitError error) {
-                System.out.println("Album failure" + error.toString());
-            }
-        });
-        
+                @Override
+                public void failure(RetrofitError error) {
+                    System.err.println("Search failure" + error.toString());
+                }
+            });
+        }
+
+
+
 
     }
 
